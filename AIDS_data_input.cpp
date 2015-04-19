74c449aed95e55703db790ba1a49e6aabf43e018
jxy <czjxy8898@gmail.com>
2015-04-19 13:12:57 +0800

    增加数据转换的容错性

--- AIDS_data_input.cpp
+++ AIDS_data_input.cpp
@@ -2,35 +2,80 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cmath>
+#include <cstring>
 using namespace std;
-bool copyToFile(FILE *file,int num)
+struct Graph
 {
-	int NumNode,NumEdge,i,a,b;
-	char NodeName[100],line[1000];
-	if(scanf("%d",&a)!=1){return false;}//判断格式正确
-	if(file!=NULL){fprintf(file,"#graph%d\n",num);}//从0开始
+	int ID;
+	int NumNode,NumEdge;
+	char (*NodeName)[10];
+	int (*EdgePair)[2];
+	bool ok;
+};
+
+bool getOneGraph(int num,Graph *graph)
+{
+	int i,a,b,NSC;
+	char line[1000];
+	bool error=false,READ=true; //error容错备用
+	int TempNode=0,TempEdge=0;//备用，格式有问题时自计数
+	if(scanf("%d",&NSC)!=1){READ=false;}//判断格式正确
 	gets(line);gets(line);gets(line);//跳过行
-	if(scanf("%d%d",&NumNode,&NumEdge)!=2){return false;}
-	if(file!=NULL){fprintf(file,"%d\n",NumNode);}
-	for(i=0;i<NumNode;i++)
-	{
-		scanf("%*f%*f%*f %s",NodeName);//按格式读取
-		gets(line);
-		if(file!=NULL){fprintf(file,"%s\n",NodeName);}
+
+	graph->ID=num;
+	if(scanf("%d%d\n",&graph->NumNode,&graph->NumEdge)!=2){
+		error=true;//格式有问题
 	}
-	if(file!=NULL){fprintf(file,"%d\n",NumEdge);}
-	for(i=0;i<NumEdge;i++)
+
+	graph->NodeName=(char (*)[10])calloc(graph->NumNode,sizeof(char[10]));
+	graph->EdgePair=(int (*)[2])calloc(graph->NumEdge,sizeof(int[2]));
+
+	for(i=0;i<graph->NumNode;i++)
 	{
-		scanf("%d%d",&a,&b);
+		if(scanf("%*f%*f%*f %s",graph->NodeName[i])!=1){
+			//TODO 格式错误
+			READ=false;
+			break;
+		};
+		gets(line);
+		TempNode++;
+	}
+	for(i=0;i<graph->NumEdge;i++)
+	{
+		if(scanf("%d%d",&a,&b)!=2){READ=false;break;}
 		gets(line);
 		a--;b--;//以0开始
-		if(file!=NULL){fprintf(file,"%d %d\n",a,b);}
+		graph->EdgePair[i][0]=a;
+		graph->EdgePair[i][1]=b;
 	}
-	gets(line);gets(line);gets(line);gets(line);gets(line);gets(line);gets(line);
-	if(file!=NULL){fprintf(file,"\n\n");}
-	return true;
+	while(strcmp(line,"$$$$")!=0)
+	{
+		if(gets(line)==NULL){break;}
+	}
+	if(READ==0){ //异常
+		printf("id:%d NSC:%d result:%d\n",num,NSC,READ);	
+	}
+	return READ;
 	
 }
+
+void outputGraph(FILE *file,Graph *graph)
+{
+	int i;
+	fprintf(file, "#graph%d\n", graph->ID);
+	fprintf(file, "%d\n", graph->NumNode);
+	for(i=0;i<graph->NumNode;i++)
+	{
+		fprintf(file,"%s\n",graph->NodeName[i]);
+	}
+	fprintf(file, "%d\n", graph->NumEdge);
+	for (i = 0; i < graph->NumEdge; ++i)
+	{
+		fprintf(file, "%d %d\n", graph->EdgePair[i][0],graph->EdgePair[i][1]);	
+	}
+	fprintf(file, "\n\n");
+}
+
 int main(int argc, char **argv)
 {
 	if(argc==1){return 0;}
@@ -42,16 +87,19 @@ int main(int argc, char **argv)
 	sprintf(OutputFileName,"%s-%d",argv[1],Num);
 	OutputFile=fopen(OutputFileName,"w");
 	int num=0;
+	Graph graph;
 	while(num<Num)
 	{
 		char c=getchar();
 		if(c==EOF){printf("No Enough! Only:%d\n",num);break;}
 		ungetc(c,stdin);
-		printf("%d\n",num);
-		FILE *now=OutputFile;
-		//if(rand()%3==0){now=NULL;}//随机跳过一些
-		if(copyToFile(now,num)==true){num++;}
-		else{gets(OutputFileName);puts(OutputFileName); break;}
+
+		if(getOneGraph(num,&graph)==true)
+		{
+			if(rand()%3==0){continue;}
+			outputGraph(OutputFile,&graph);
+			num++;
+		}
 
 	}
 	fclose(OutputFile);
